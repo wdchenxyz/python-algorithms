83c79250542ecdcd7704e6eaa9c1283ebee2755c
wdchenxyz <wdchen@datalab.cs.nthu.edu.tw>
2018-06-02 01:43:11 +0800

    subtract using bitwise operator

--- bits/arithmetic_bitswise_operator.py
+++ bits/arithmetic_bitswise_operator.py
@@ -9,8 +9,17 @@ Output:
 
 def add_bitwise_operator(x, y):
     ''' x + y using bitwise operator
-        Idea: 302 + 099 = 391 + 010 = 301 + 100 = 401
-        Split the non-carry bits and carry bits.
+        Idea: Split the non-carry bits and carry bits.
+        302 + 099 = 391 + 010 = 301 + 100 = 401 + 000
+
+        x     y     sum   carry
+        0     0     0     0
+        0     1     1     0
+        1     0     1     0
+        1     1     0     1
+
+        sum = x ^ y
+        carry = x & y
     '''
     
     while y: 
@@ -20,5 +29,25 @@ def add_bitwise_operator(x, y):
     return x
 
 def subtract_bitwise_operator(x, y):
-    pass
+    ''' x - y using bitwise operator
+        Idea: 
+        302 - 099 = 313 - 110 = 203 - 000
+        split the non-borrow bits and borrow bits
+
+        x    y    diff  borrow    
+        0    0    0     0
+        0    1    1     1
+        1    0    1     0
+        1    1    0     0
+
+        diff = x ^ y 
+        borrow = (~x) & y
+    '''
+    while y: 
+        borrow = (~x) & y # find which bits will result in borrow bit
+        x = x ^ y # use XOR to subtract the bits without producing a borrow bit
+        y = borrow << 1 # carry the borrow bits one column left
+    return x
+
+
     
