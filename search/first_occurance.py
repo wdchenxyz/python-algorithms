c374ee1f3d306dc64112318a9eb1481ae676004f
wdchenxyz <wdchen@datalab.cs.nthu.edu.tw>
2018-06-12 17:33:06 +0800

    update

--- search/first_occurance.py
+++ search/first_occurance.py
@@ -4,18 +4,17 @@
 # T(n): O(log n)
 """
 
-def first_occurance(array, query):
-    lo, hi = 0, len(array) - 1
-    while lo <= hi:
+def first_occurance(nums, target):
+    lo, hi = 0, len(nums) - 1
+    while lo < hi:
         mid = (lo + hi) // 2
-        if array[mid] == query:
-            return mid
-        if array[mid] < query:
+        if nums[mid] < target:
             lo = mid + 1
-        else :
-            hi = mid - 1
-    
+        else:
+            hi = mid
+    if (nums[lo] == target):
+        return lo
     return -1
 
-case1 = [1, 2, 4, 5, 7, 8, 9]
+case1 = [1, 2, 4, 5, 5, 5, 5, 5, 7, 8, 9]
 print(first_occurance(case1, 5))
