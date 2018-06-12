57c912b27bc01e1db2fcebcfb3d6f991e573c515
wdchenxyz <wdchen@datalab.cs.nthu.edu.tw>
2018-06-12 17:06:09 +0800

    add recursion

--- search/binary_search.py
+++ search/binary_search.py
@@ -16,7 +16,19 @@ def binary_search(array, query):
         else :
             hi = mid - 1
     
-    return None
+    return -1
+
+def binary_search_recur(array, low, high, query):
+    if low > high:
+        return -1
+    mid = (low + high) // 2
+    if array[mid] < query:
+        return binary_search_recur(array, mid+1, high, query)
+    elif array[mid] > query:
+        return binary_search_recur(array, low, mid-1, query)
+    else:
+        return mid
 
 case1 = [1, 2, 4, 5, 7, 8, 9]
-print(binary_search(case1, 9))
+print(binary_search(case1, 5))
+print(binary_search_recur(case1, 0, len(case1)-1, 5))
